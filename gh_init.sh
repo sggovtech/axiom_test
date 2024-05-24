9f7193cdf27ddac2aeac6d0ec800784840d6256c
sggovtech <166391274+sggovtech@users.noreply.github.com>
2024-05-24 09:07:07 +0000

    more changes

--- gh_init.sh
+++ gh_init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if ! command -v gh &> /dev/null; then
     echo "gh is not installed. Installing..."
-    (type -p wget >/dev/null || (sudo apt update && sudo apt-get install wget -y)) \
+    (type -p wget >/dev/null || (sudo apt update && sudo apt-get install git wget -y)) \
     && sudo mkdir -p -m 755 /etc/apt/keyrings \
     && wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
     && sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
@@ -9,5 +9,8 @@ if ! command -v gh &> /dev/null; then
     && sudo apt update \
     && sudo apt install gh -y
 fi
-gh auth login
-gh repo create "$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 10 | head -n 1)"
+unset GITHUB_TOKEN
+gh auth login --scopes delete_repo 
+repo_name=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 10 | head -n 1)
+gh repo create $repo_name --public
+gh repo delete $repo_name --yes
